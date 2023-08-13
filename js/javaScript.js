57748a869a54ac52483298a2750f3832ca43f8d8
HAguilarCarranza <heberth.aguilar.@gmail.com>
2023-08-12 23:09:22 -0500

    Formulario Completo

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,9 +1,10 @@
 const formulario = document.getElementById('form')
 const inputs = document.querySelectorAll('#form input')
+const btn = document.getElementById('button');
 
 const expresiones = {
-	Name: /^.{4,100}$/, 
-	Email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    Name: /^.{1,100}$/,
+    Email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
 
 const campos = {
@@ -11,82 +12,73 @@ const campos = {
     Email: false
 }
 
-
 const validarFormulario = (e) => {
-    switch (e.target.name){
+    switch (e.target.name) {
         case "name":
-            validarCampo(expresiones.Name, e.target,'Name');
-        break
+            validarCampo(expresiones.Name, e.target, 'Name');
+            break
         case "email":
             validarCampo(expresiones.Email, e.target, 'Email')
-        break
+            break
     }
 }
 
-
-const validarCampo = (exprecion,input,campo) =>{
-    if(exprecion.test(input.value)){
+const validarCampo = (exprecion, input, campo) => {
+    if (exprecion.test(input.value)) {
         document.getElementById(`grupo${campo}`).classList.remove('grupoIncorreto')
         document.getElementById(`grupo${campo}`).classList.add('grupoCorreto')
         document.querySelector(`#grupo${campo} span`).classList.remove('fa-circle-xmark')
         document.querySelector(`#grupo${campo} span`).classList.add('fa-circle-check')
-        campos[campo]=true
+        campos[campo] = true
     }
     else {
         document.getElementById(`grupo${campo}`).classList.remove('grupoCorreto')
         document.getElementById(`grupo${campo}`).classList.add('grupoIncorreto')
         document.querySelector(`#grupo${campo} span`).classList.remove('fa-circle-check')
         document.querySelector(`#grupo${campo} span`).classList.add('fa-circle-xmark')
-        campos[campo]=false
+        campos[campo] = false
     }
 }
 
 inputs.forEach((input) => {
-    input.addEventListener('keyup',validarFormulario)
-    input.addEventListener('blur',validarFormulario)
+    input.addEventListener('keyup', validarFormulario)
+    input.addEventListener('blur', validarFormulario)
 })
-
-
-
-formulario.addEventListener('submit',(e) => {
-
-    if (campos.Name && campos.Email){
-        formulario.reset();
-    }
-    else{
-
-    }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-const btn = document.getElementById('button');
 
 document.getElementById('form')
     .addEventListener('submit', function (event) {
-        event.preventDefault();
 
-        btn.value = 'Enviando...';
+        if (campos.Name && campos.Email) {
 
-        const serviceID = 'default_service';
-        const templateID = 'template_7hbmeex';
+            event.preventDefault();
 
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                btn.value = 'Enviar Mensaje';
-                alert('Mensaje Enviado!');
-            }, (err) => {
-                btn.value = 'Enviar Mensaje';
-                alert('Error al enviar Mensaje!');
-                alert(JSON.stringify(err));
-            });
+            btn.value = 'Enviando...';
+
+            const serviceID = 'default_service';
+            const templateID = 'template_7hbmeex';
+
+            emailjs.sendForm(serviceID, templateID, this)
+                .then(() => {
+                    btn.value = 'Enviar Mensaje';
+                    document.getElementById('textoFormularioError').classList.remove('textoFormularioEnviadoAvtivo')
+                    document.getElementById('textoFormularioEnviado').classList.add('textoFormularioEnviadoAvtivo')
+                    setTimeout(() => {document.getElementById('textoFormularioEnviado').classList.remove('textoFormularioEnviadoAvtivo')}, 5000);
+                    document.querySelectorAll('.grupoCorreto').forEach((icono) => {icono.classList.remove('grupoCorreto');})
+                    formulario.reset()
+                }, (err) => {
+                    btn.value = 'Enviar Mensaje';
+                    alert('Error al enviar Mensaje!');
+                    alert(JSON.stringify(err));
+                });
+            campos.Name = false
+            campos.Email = false
+        }
+        else {
+            event.preventDefault();
+            document.getElementById('textoFormularioEnviado').classList.remove('textoFormularioEnviadoAvtivo')
+            document.getElementById('textoFormularioError').classList.add('textoFormularioEnviadoAvtivo')
+            setTimeout(() => {
+                document.getElementById('textoFormularioError').classList.remove('textoFormularioEnviadoAvtivo')
+            }, 5000);
+        }
     });

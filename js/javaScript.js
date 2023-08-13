f0c97360dcbf83761fb4be7a1b5d8868ef9e060e
HAguilarCarranza <heberth.aguilar.@gmail.com>
2023-08-12 20:58:32 -0500

    Formulario al 90%

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,3 +1,73 @@
+const formulario = document.getElementById('form')
+const inputs = document.querySelectorAll('#form input')
+
+const expresiones = {
+	Name: /^.{4,100}$/, 
+	Email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
+
+const campos = {
+    Name: false,
+    Email: false
+}
+
+
+const validarFormulario = (e) => {
+    switch (e.target.name){
+        case "name":
+            validarCampo(expresiones.Name, e.target,'Name');
+        break
+        case "email":
+            validarCampo(expresiones.Email, e.target, 'Email')
+        break
+    }
+}
+
+
+const validarCampo = (exprecion,input,campo) =>{
+    if(exprecion.test(input.value)){
+        document.getElementById(`grupo${campo}`).classList.remove('grupoIncorreto')
+        document.getElementById(`grupo${campo}`).classList.add('grupoCorreto')
+        document.querySelector(`#grupo${campo} span`).classList.remove('fa-circle-xmark')
+        document.querySelector(`#grupo${campo} span`).classList.add('fa-circle-check')
+        campos[campo]=true
+    }
+    else {
+        document.getElementById(`grupo${campo}`).classList.remove('grupoCorreto')
+        document.getElementById(`grupo${campo}`).classList.add('grupoIncorreto')
+        document.querySelector(`#grupo${campo} span`).classList.remove('fa-circle-check')
+        document.querySelector(`#grupo${campo} span`).classList.add('fa-circle-xmark')
+        campos[campo]=false
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup',validarFormulario)
+    input.addEventListener('blur',validarFormulario)
+})
+
+
+
+formulario.addEventListener('submit',(e) => {
+
+    if (campos.Name && campos.Email){
+        formulario.reset();
+    }
+    else{
+
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
 
 const btn = document.getElementById('button');
 
